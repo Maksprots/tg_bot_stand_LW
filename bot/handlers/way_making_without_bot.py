711ff6a99e4a94d37beb236c4a3b5bc85b27feaf
Бутенко Елизавета <esbutenko@miem.hse.ru>
2023-01-22 15:16:50 +0300

    Исправлены замечания

--- bot/handlers/way_making_without_bot.py
+++ bot/handlers/way_making_without_bot.py
@@ -5,15 +5,18 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 from aiogram.dispatcher import FSMContext
 from bot import markups as mp
 import yaml
-
+LANGUAGE = 'RU'
 filename = 'bot\\static\\texts\\answers_text_way2.yaml'
+
 
 def read_from_yaml(filepath):
     with open(filepath, encoding='utf-8') as fh:
         dictionary_data = yaml.safe_load(fh)
-        return dictionary_data['RU']
+        return dictionary_data
+
 
 dictionary_yaml_answers = read_from_yaml(filename)
+
 
 # Отмена загрузки файлов
 async def cancel_load_w2(message: types.Message, state: FSMContext) -> None:
@@ -21,7 +24,8 @@ async def cancel_load_w2(message: types.Message, state: FSMContext) -> None:
     if current_state is None:
         return
     await message. \
-        reply(dictionary_yaml_answers['back_to_menu'], reply_markup=mp.menu)
+        reply(dictionary_yaml_answers[LANGUAGE]['back_to_menu'],
+              reply_markup=mp.menu)
     await state.finish()
 
 
@@ -36,14 +40,14 @@ class ClientStatesGroup2(StatesGroup):
 async def choose_way(message: types.Message) -> None:
     await ClientStatesGroup2.nothing.set()
     await message \
-        .answer(dictionary_yaml_answers['chosen_way_without_bot'],
+        .answer(dictionary_yaml_answers[LANGUAGE]['chosen_way_without_bot'],
                 reply_markup=mp.load_firmware)
 
 
 # отправка пользователем прошивки
 async def start_load_board(message: types.Message) -> None:
     await ClientStatesGroup2.board.set()
-    await message.answer(dictionary_yaml_answers['add_file_sof'])
+    await message.answer(dictionary_yaml_answers[LANGUAGE]['add_file_sof'])
 
 
 # Сохранение прошивки
@@ -52,21 +56,21 @@ async def load_board(message: types.Message):
     board_id = message.document.file_id
     board_info = await bot.get_file(board_id)
     await message.document.download(board_info.file_path)
-    await message.answer(dictionary_yaml_answers['success_load'],
+    await message.answer(dictionary_yaml_answers[LANGUAGE]['success_load'],
                          reply_markup=mp.load_script)
 
 
 async def start_load_desc(message: types.Message) -> None:
     await ClientStatesGroup2.desc.set()
-    await message.answer(dictionary_yaml_answers['script_loading'])
+    await message.answer(dictionary_yaml_answers[LANGUAGE]['script_loading'])
 
 
 async def scan_message(message: types.Message, state: FSMContext):
     document_id = message.document.file_id
     file_info = await bot.get_file(document_id)
     await message.document.download(file_info.file_path)
-    await message.answer(dictionary_yaml_answers['file_saving'])
-    await message.answer(dictionary_yaml_answers['wait_letter'],
+    await message.answer(dictionary_yaml_answers[LANGUAGE]['file_saving'])
+    await message.answer(dictionary_yaml_answers[LANGUAGE]['wait_letter'],
                          reply_markup=mp.menu)
     await state.finish()
 
