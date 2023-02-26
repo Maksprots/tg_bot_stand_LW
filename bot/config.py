02993041ccd76ceef0bfbf9b69099cca71eb4eae
Maksprots <makslive@bk.ru>
2023-02-26 15:12:57 +0300

    Большой рефакторинг пути без подсказк, добавлена правильная закачка файлов и убраны deprecation warning, и пару грубых ошибок недающих функционировать

---
+++ bot/config.py
@@ -0,0 +1,3 @@
+path_for_users_script = 'documents/S{}.txt'
+path_for_users_firmware = 'documents/F{}.sof'
+TEXT_PATH_WITHOUT = '/static/texts/answers_text_without_bot.yaml'
--- bot/handlers/way_making_without_bot.py
+++ bot/handlers/way_making_without_bot.py
@@ -7,15 +7,24 @@ from bot import markups as mp
 import yaml
 import os
 
+from bot.config import path_for_users_firmware, path_for_users_script
+from bot.config import TEXT_PATH_WITHOUT
 
-TEXT_PATH = '/static/texts/answers_text_without_bot.yaml'
+
 LANGUAGE = 'RU'
-with open(os.getcwd() + TEXT_PATH, encoding='utf-8') as fh:
+with open(os.getcwd() + TEXT_PATH_WITHOUT, encoding='utf-8') as fh:
     dictionary_yaml_answers = yaml.safe_load(fh)
 
 
+class ClientStatesGroup2(StatesGroup):
+    nothing = State()
+    board = State()
+    desc = State()
+    mail = State()
+
+
 # Отмена загрузки файлов
-async def cancel_load_w2(message: types.Message, state: FSMContext) -> None:
+async def cancel_upload_w2(message: types.Message, state: FSMContext) -> None:
     current_state = await state.get_state()
     if current_state is None:
         return
@@ -25,31 +34,23 @@ async def cancel_load_w2(message: types.Message, state: FSMContext) -> None:
     await state.finish()
 
 
-# назначение состояний
-class ClientStatesGroup2(StatesGroup):
-    nothing = State()
-    board = State()
-    desc = State()
-    mail = State()
-
-
 async def ask_for_email(message: types.Message) -> None:
     await ClientStatesGroup2.mail.set()
     await message.answer(dictionary_yaml_answers[LANGUAGE]['ask_email'])
 
 
 # Сохранение почты
-async def save_mail(message: types.Message,  state: FSMContext):
+async def save_email_addr(message: types.Message, state: FSMContext):
     user_email = message.text
-    if not(user_email[len(user_email)-len('@edu.hse.ru'):] == '@edu.hse.ru'
-           and user_email.count('@') == 1 and user_email != '@edu.hse.ru'):
+    if not (user_email[len(user_email) - len('@edu.hse.ru'):] == '@edu.hse.ru'
+            and user_email.count('@') == 1 and user_email != '@edu.hse.ru'):
         await ask_for_email(message)
-        await save_mail(message)
+        return
     await state.update_data(email=user_email)
-    await choose_way(message=message)
+    await chosen_way(message=message)
 
 
-async def choose_way(message: types.Message) -> None:
+async def chosen_way(message: types.Message) -> None:
     await ClientStatesGroup2.nothing.set()
     await message \
         .answer(dictionary_yaml_answers[LANGUAGE]['chosen_way_without_bot'],
@@ -57,33 +58,37 @@ async def choose_way(message: types.Message) -> None:
 
 
 # отправка пользователем прошивки
-async def start_load_board(message: types.Message) -> None:
+async def invitation_to_upload_firmware(message: types.Message) -> None:
     await ClientStatesGroup2.board.set()
     await message.answer(dictionary_yaml_answers[LANGUAGE]['add_file_sof'])
 
 
 # Сохранение прошивки
-async def load_board(message: types.Message):
-    # TODO: написать сборку зипфайла с названием по тг айди
+async def downloading_firmware(message: types.Message):
     await ClientStatesGroup2.board.set()
-    board_id = message.document.file_id
-    board_info = await bot.get_file(board_id)
-    await message.document.download(board_info.file_path)
+    await message.document.download(
+        destination_file=path_for_users_firmware.format(
+            message.from_user.id
+        )
+    )
     await message.answer(dictionary_yaml_answers[LANGUAGE]['success_load'],
                          reply_markup=mp.load_script)
 
 
-async def start_load_desc(message: types.Message) -> None:
+async def invitation_to_upload_script(message: types.Message) -> None:
     await ClientStatesGroup2.desc.set()
     await message.answer(dictionary_yaml_answers[LANGUAGE]['script_loading'])
 
 
-async def scan_message(message: types.Message, state: FSMContext):
-    document_id = message.document.file_id
+async def downloading_of_script(message: types.Message, state: FSMContext):
     # TODO: вызов отправки зипника на гугл драйв
-    file_info = await bot.get_file(document_id)
     data = await state.get_data()
-    await message.document.download(file_info.file_path)
+    await message.document.download(
+        destination_file=
+        path_for_users_script.format(
+            message.from_user.id
+        )
+    )
     await message.answer(dictionary_yaml_answers[LANGUAGE]['file_saving'])
     await message.answer(dictionary_yaml_answers[LANGUAGE]['wait_letter'] + " "
                          + data['email'],
@@ -93,21 +98,21 @@ async def scan_message(message: types.Message, state: FSMContext):
 
 def registration_of_handlers(dispatcher: Dispatcher):
     dispatcher.register_message_handler(
-        cancel_load_w2, commands=["cancel"], state="*")
+        cancel_upload_w2, commands=["cancel"], state="*")
     dispatcher.register_message_handler(
         ask_for_email, Text(equals="Способ 2", ignore_case=True), state="*")
     dispatcher.register_message_handler(
-        save_mail,
+        save_email_addr,
         state=ClientStatesGroup2.mail.state)
     dispatcher.register_message_handler(
-        start_load_board, Text(equals="Загрузка прошивки", ignore_case=True),
+        invitation_to_upload_firmware, Text(equals="Загрузка прошивки", ignore_case=True),
         state="*")
     dispatcher.register_message_handler(
-        load_board, content_types=['document'],
+        downloading_firmware, content_types=['document'],
         state=ClientStatesGroup2.board.state)
     dispatcher.register_message_handler(
-        start_load_desc, Text(equals="Загрузить cценарий", ignore_case=True),
+        invitation_to_upload_script, Text(equals="Загрузить cценарий", ignore_case=True),
         state="*")
     dispatcher.register_message_handler(
-        scan_message, content_types=['document'],
+        downloading_of_script, content_types=['document'],
         state=ClientStatesGroup2.desc.state)
